38616863f4f3e1e0bcd3df892e4c33e35dc4975c
gyuta <kuroshiba0408@gmail.com>
2021-03-16 14:00:16 +0900

    インジケータの位置を調整

--- lib/recommend.dart
+++ lib/recommend.dart
@@ -17,19 +17,40 @@ class _RecommendState extends State<Recommend> {
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //     body: Center(
+    //         child: Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   children: [
+    //     Padding(
+    //         padding: const EdgeInsets.all(8.0),
+    //         child: Judge(callback: setProgress)),
+    //     LinearProgressIndicator(
+    //       value: progress,
+    //       minHeight: 40,
+    //     )
+    //   ],
+    // )));
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
+        body: Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Positioned(
+            height: 50.0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: LinearProgressIndicator(
+              value: progress,
+              // minHeight: 40,
+            )),
         Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Judge(callback: setProgress)),
-        LinearProgressIndicator(
-          value: progress,
-          minHeight: 40,
-        )
+          padding: const EdgeInsets.all(10.0),
+          child: Judge(
+            callback: setProgress,
+          ),
+        ),
       ],
-    )));
+    ));
   }
 }
