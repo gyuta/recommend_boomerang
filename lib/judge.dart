2b0d05df9a4394035243623b813829e8a4a71956
gyuta <kuroshiba0408@gmail.com>
2021-03-16 12:46:54 +0900

    リファクタ

--- lib/judge.dart
+++ lib/judge.dart
@@ -21,24 +21,31 @@ class _JudgeState extends State<Judge> {
     return Stack(
       children: [
         Text(point.toString()),
-        Choice(
-            title: "我慢強いほうだ",
-            img:
-                "https://3.bp.blogspot.com/-CqYmAclTr3Y/VlaCvXbhDGI/AAAAAAAA1B8/evIAehX02pA/s180-c/medical_geppu_gaman.png",
-            callback: callBack),
-        Choice(
-          title: "人を殴ったことがある",
-          img:
-              "https://1.bp.blogspot.com/-Ud3Ndr6_SD4/WZVg5YDdQSI/AAAAAAABGFw/FTym1iJJqqkSaFVxbLHR3gOe0-oALH7XwCLcBGAs/s400/kenka_karamu_man.png",
-          callback: callBack,
-        ),
-        Choice(
-          title: "野球経験がある",
-          img:
-              "https://3.bp.blogspot.com/-Gr6f-H1rXko/Wi4fz7xuGdI/AAAAAAABIq4/_ngtRKC44XwlZTul6ex_lev2lDOEN1TKwCLcBGAs/s450/baseball_pitcher_overthrow.png",
-          callback: callBack,
-        ),
+        for (var i in cardContent)
+          Choice(
+            title: i["title"],
+            img: i["img"],
+            callback: callBack,
+          )
       ],
     );
   }
 }
+
+const List<Map<String, String>> cardContent = [
+  {
+    "title": "我慢強いほうだ",
+    "img":
+        "https://3.bp.blogspot.com/-CqYmAclTr3Y/VlaCvXbhDGI/AAAAAAAA1B8/evIAehX02pA/s180-c/medical_geppu_gaman.png"
+  },
+  {
+    "title": "人を殴ったことがある",
+    "img":
+        "https://1.bp.blogspot.com/-Ud3Ndr6_SD4/WZVg5YDdQSI/AAAAAAABGFw/FTym1iJJqqkSaFVxbLHR3gOe0-oALH7XwCLcBGAs/s400/kenka_karamu_man.png"
+  },
+  {
+    "title": "野球経験がある",
+    "img":
+        "https://3.bp.blogspot.com/-Gr6f-H1rXko/Wi4fz7xuGdI/AAAAAAABIq4/_ngtRKC44XwlZTul6ex_lev2lDOEN1TKwCLcBGAs/s450/baseball_pitcher_overthrow.png"
+  },
+];
